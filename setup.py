bac8c0bbe289dcaa13f08e77319ef140c82e1f93
koulali <achraf.koulali@ncl.ac.uk>
2024-01-28 10:20:08 +0000

    update 0.1

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/koulali/pycegm',
-    version='0.0.9',
+    version='0.1',
     zip_safe=False,
 )
